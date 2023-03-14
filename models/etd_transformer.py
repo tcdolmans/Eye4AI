dd3d8295ca73ddf57c078ebfc22a0c6232e9be57
Tenzing Dolmans <t.c.dolmans@gmail.com>
2023-03-14 15:12:41 +0200

    Committing changes

---
+++ models/etd_transformer.py
@@ -0,0 +1,262 @@
+import sys
+import os
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+sys.path.insert(1, os.path.join(sys.path[0], '../'))
+from pipeline.utils import list_files # noqa
+from pipeline.dataset_constructor import return_data_loaders, replace_nans # noqa
+
+
+class MLP(nn.Module):
+    def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=nn.GELU, drop=0):
+        super().__init__()
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
+        self.fc1 = nn.Linear(in_features, hidden_features)
+        self.act = act_layer()
+        self.fc2 = nn.Linear(hidden_features, out_features)
+        self.drop = nn.Dropout(drop)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.act(x)
+        x = self.drop(x)
+        x = self.fc2(x)
+        x = self.drop(x)
+        return x
+
+
+class ETPatchEmbed(nn.Module):
+    """
+    embed_dim = 250
+    1. Split the input data into 12 patches (3 seconds)
+    2. Get linear embedding of each patch, shape = (2, 250) --> (1, 500)
+       All patches for sample = 12, 500
+    3. Add position embedding and [cls] to linear embedding (12, 500) --> (13, 500)
+    """
+    def __init__(self, sample_height=3000, sample_width=3, patch_size=125, in_chans=64, embed_dim=12):
+        super().__init__()
+        sample_size = tuple((sample_height, sample_width))
+        patch_size = tuple((patch_size, sample_width))
+        num_patches = (sample_size[1] // patch_size[1] * sample_size[0] // patch_size[0])
+        self.sample_size = sample_size
+        self.patch_size = patch_size
+        self.num_patches = num_patches
+        self.proj = nn.Conv2d(in_channels=in_chans, out_channels=embed_dim, kernel_size=patch_size, stride=patch_size)
+        self.conv_layer = nn.Conv1d(in_channels=3, out_channels=48, kernel_size=100, stride=100)
+
+    def forward(self, x):
+        # B, C, H, W = x.shape
+        # assert H == self.sample_size[0] and W == self.sample_size[1], \
+        #     f"Input image size ({H}*{W}) doesn't match model ({self.sample_size[0]}*{self.sample_size[1]})."
+        # print(x.shape)
+        x = self.conv_layer(x.permute(0, 2, 1))
+        # print(x.shape)
+        return x
+
+
+class ETMLP(nn.Module):
+    def __init__(self, input_size=9216, hidden_size=4096, num_classes=31,
+                 patch_size=100, in_chans=4, embed_dim=12):
+        super().__init__()
+        self.patch_embed = ETPatchEmbed(
+            sample_height=3000,
+            sample_width=4,
+            patch_size=125,
+            in_chans=4,
+            embed_dim=12)
+        self.flat = nn.Flatten()
+        self.fc1 = nn.Linear(9000, 2048)
+        self.relu1 = nn.ReLU()
+        self.fc2 = nn.Linear(2048, 1024)
+        self.relu2 = nn.ReLU()
+        self.fc3 = nn.Linear(1024, 64)
+        self.relu3 = nn.ReLU()
+        self.fc4 = nn.Linear(64, 1)
+        self.soft = nn.Softmax(dim=0)
+        # Change to output batch_size, num_classes
+        # Softmax for top 5?
+
+    def forward(self, x):
+        x = x.to(torch.device('cuda'))
+        x = torch.cuda.FloatTensor(x.float())
+        # out = self.patch_embed(x)
+
+        out = self.flat(x)
+        out = self.fc1(out)
+        out = self.relu1(out)
+        out = self.fc2(out)
+        out = self.relu2(out)
+        out = self.fc3(out)
+        out = self.relu3(out)
+        out = self.fc4(out)
+        out = self.soft(out)
+        return out
+
+
+class ETAttention(nn.Module):
+    def __init__(self, dim, num_heads=8, qkv_bias=False, qk_scale=None, attn_drop=0., proj_drop=0.):
+        super().__init__()
+        self.num_heads = num_heads
+        head_dim = dim // num_heads
+        # NOTE scale factor can be set manually to be compat with prev weights
+        self.scale = qk_scale or head_dim ** -0.5
+        self.qkv = nn.Linear(dim, dim * 3, bias=qkv_bias)
+        self.attn_drop = nn.Dropout(attn_drop)
+        self.proj = nn.Linear(dim, dim)
+        self.proj_drop = nn.Dropout(proj_drop)
+
+    def forward(self, x):
+        B, N, C = x.shape
+        qkv = self.qkv(x).reshape(B, N, 3, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)
+        q, k, v = qkv[0], qkv[1], qkv[2]   # torchscript cannot use tensor as tuple
+
+        attn = (q @ k.transpose(-2, -1)) * self.scale
+        attn = attn.softmax(dim=-1)
+        attn = self.attn_drop(attn)
+
+        x = (attn @ v).transpose(1, 2).reshape(B, N, C)
+        x = self.proj(x)
+        x = self.proj_drop(x)
+        return x
+
+
+class Block(nn.Module):
+    def __init__(self, dim, num_heads, mlp_ratio=4., qkv_bias=False, qk_scale=None, drop=0., attn_drop=0.,
+                 act_layer=nn.GELU, norm_layer=nn.LayerNorm):
+        super().__init__()
+        self.norm1 = norm_layer(dim)
+        self.attn = ETAttention(
+            dim, num_heads=num_heads, qkv_bias=qkv_bias, qk_scale=qk_scale, attn_drop=attn_drop, proj_drop=drop)
+        self.drop = nn.Dropout(attn_drop)
+        self.norm2 = norm_layer(dim)
+        mlp_hidden_dim = int(dim * mlp_ratio)
+        self.mlp = MLP(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)
+
+    def forward(self, x):
+        x = x + self.drop(self.attn(self.norm1(x)))
+        x = x + self.drop(self.mlp(self.norm2(x)))
+        return x
+
+
+class ETTransformer(nn.Module):
+    def __init__(self, sample_height=3000, sample_width=4, patch_size=250, in_chans=4,
+                 num_classes=31, embed_dim=12, depth=4, num_heads=4, mlp_ratio=4.,
+                 qkv_bias=False, qk_scale=None, drop_rate=0., attn_drop_rate=0.,
+                 drop_path_rate=0., norm_layer=nn.LayerNorm):
+        super().__init__()
+        self.num_classes = num_classes
+        self.num_features = self.embed_dim = embed_dim
+        self.patch_embed = ETPatchEmbed(sample_height=3000, sample_width=4, patch_size=patch_size,
+                                        in_chans=in_chans, embed_dim=embed_dim)
+        num_patches = self.patch_embed.num_patches
+        self.cls_token = nn.Parameter(torch.zeros(1, 1, embed_dim))
+        self.pos_embed = nn.Parameter(torch.zeros(1, num_patches + 1, embed_dim))
+        self.pos_drop = nn.Dropout(p=drop_rate)
+        self.blocks = nn.ModuleList([
+            Block(
+                  dim=embed_dim, num_heads=num_heads, mlp_ratio=mlp_ratio, qkv_bias=qkv_bias,
+                  qk_scale=qk_scale, drop=drop_rate, attn_drop=attn_drop_rate, norm_layer=norm_layer)
+            for i in range(depth)])
+        self.norm = norm_layer(embed_dim)
+        self.head = nn.Linear(embed_dim, num_classes) if num_classes > 0 else nn.Identity()
+
+    def forward_features(self, x):
+        B = x.shape[0]
+        x = self.patch_embed(x)
+
+        cls_tokens = self.cls_token.expand(B, -1, -1)
+        x = torch.cat((cls_tokens, x), dim=1)
+        x = x + self.pos_embed
+        x = self.pos_drop(x)
+
+        for blk in self.blocks:
+            x = blk(x)
+
+        x = self.norm(x)
+        return x[:, 0]
+
+    def forward(self, x):
+        x = self.forward_features(x)
+        x = self.head(x)
+        return x
+
+
+def train_one_epoch(epoch_index):
+    running_loss = 0
+    last_loss = 0
+
+    # Here, we use enumerate(training_loader) instead of
+    # iter(training_loader) so that we can track the batch
+    # index and do some intra-epoch reporting.
+    for i, data in enumerate(train_dataloader):
+        labels = data[:, 0, 0].float()
+        labels = labels.to(torch.device('cuda'))
+        inputs = data[:, 1:, 1:]  # Whole batch, exclude label, exclude timestamps
+        inputs = replace_nans(inputs)
+        # conv_layer = nn.Conv1d(in_channels=3, out_channels=12, kernel_size=4, stride=250)
+        # output_tensor = conv_layer(inputs.permute(0, 2, 1).float())
+        # print(output_tensor)
+
+        optimiser.zero_grad()
+        outputs = model(inputs).squeeze()
+        # outputs = torch.tensor([list(out).index(max(out)) for out in outputs])
+        # outputs = outputs.to(torch.device('cuda'))
+        # outputs = torch.cuda.FloatTensor(outputs.float())
+        # print(outputs, labels)
+        loss = loss_fn(outputs, labels)
+        # loss.requires_grad = True
+        loss.backward()
+        optimiser.step()
+        running_loss += loss.item()
+        if i % 15 == 14:
+            last_loss = running_loss / 1000
+            print('  batch {} loss: {}'.format(i + 1, last_loss))
+            running_loss = 0.
+    return last_loss
+
+
+def train_model(model, EPOCHS, init_loss):
+    for epoch in range(EPOCHS):
+        print('EPOCH {}:'.format(epoch + 1))
+
+        # Make sure gradient tracking is on, and do a pass over the data
+        model.train(True)
+        avg_loss = train_one_epoch(epoch_number)
+        # We don't need gradients on to do reporting
+        model.train(False)
+
+        running_vloss = 0.0
+        for i, vdata in enumerate(test_dataloader):
+            vlabels = vdata[:, 0, 0].float()
+            vlabels = vlabels.to(torch.device('cuda'))
+            vinputs = vdata[:, 1:, 1:]  # Whole batch, exclude label, exclude timestamps
+            vinputs = replace_nans(vinputs)
+            voutputs = model(vinputs).squeeze()
+            # voutputs = torch.tensor([list(out).index(max(out)) for out in voutputs])
+            # voutputs = voutputs.to(torch.device('cuda'))
+            # voutputs = torch.cuda.FloatTensor(voutputs.float())
+            vloss = loss_fn(voutputs, vlabels)
+            running_vloss += vloss
+
+        avg_test_loss = running_vloss / (i + 1)
+        print('LOSS train {} valid {}'.format(avg_loss, avg_test_loss))
+
+
+if __name__ == "__main__":
+    model = ETMLP()
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    model = model.to(device)
+    # print(summary(model, (3000, 4)))
+    loss_fn = nn.CrossEntropyLoss()
+    optimiser = optim.Adam(model.parameters(), lr=1e-3, capturable=True)
+    folder = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'Pipeline', 'tensors'))
+    files = list_files(folder)
+    train_dataloader, test_dataloader = return_data_loaders(files, 124, 2)
+
+    epoch_number = 0
+    EPOCHS = 5
+    best_test_loss = 1_000_000.
+    train_model(model, EPOCHS, best_test_loss)
---
+++ pipeline/dataset_constructor.py
@@ -0,0 +1,187 @@
+import os
+import sys
+import torch
+import numpy as np
+from scipy import stats
+from torch.utils.data import Dataset, DataLoader
+sys.path.insert(1, os.path.join(sys.path[0], '../'))
+from pipeline.utils import list_files # noqa
+import time
+
+
+def time_it(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        print(f"{func.__name__} took {(end - start):.6f} seconds")
+        return result
+    return wrapper
+
+
+def alert_deprication(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        print(f"{func.__name__} depricated and took {(end - start):.6f} seconds")
+        return result
+    return wrapper
+
+
+def replace_nans(input_data, need_sanity_check=False):
+    """
+    Takes some input tensor suitable for PyTorch, then linearly interpolates the Nans away.
+    This works decently well, but can be improved by beign tested for speed.
+    """
+    # Below assumes that the input dimension you care to evaluate by
+    # will be returned by simply calling len().
+    # TODO: Generalise to differently ranked inputs.
+    # @time_it
+    def pad_with_interp(data):
+        bad_indexes = np.isnan(data)
+        good_indexes = np.logical_not(bad_indexes)
+        good_data = data[good_indexes]
+        if len(good_data) == 0:
+            return False
+        elif len(good_data) / len(data) < 0.85:  
+            #  The sample is rejected because it did not met the threshold of .85, or 85% of the evaluated over dimensions.
+            return False
+        # Craft linearly interpolated data over the seleceted data. Please refer to np.interp() when in doubt.
+        interpolated = np.interp(bad_indexes.nonzero()[0], good_indexes.nonzero()[0], good_data)
+        data[bad_indexes] = interpolated
+        return data
+
+    output = torch.tensor(np.apply_along_axis(pad_with_interp, 0, input_data))
+    if need_sanity_check:
+        print(output.shape)
+    return output
+
+
+# @time_it
+def downsample(input_tensor, dsf=10):
+    """
+    Takes input_tensor and donsamples the data by down_sampling_frequency(dsf) by return a shortened array
+    which contains the mode for each of the sample windows. Can be altered to return !mode, e.g.,
+    mean, distribution, etc.
+    Little bit of unpacking and looping, hence the decorator. Please remember to switch it off when you are not testing
+
+    """
+    ds = []
+    for i in range(0, len(input_tensor), dsf):
+        selection = stats.mode(input_tensor[i: i+dsf], axis=0, keepdims=True)[0][0]
+        ds.append(torch.tensor(np.array(selection)).unsqueeze(0))
+    return torch.cat(ds)
+
+
+# @time_it
+def downsample_task_tensor(file, dsf=10):
+    tensor = torch.load(file)
+    for i, t in enumerate(tensor):
+        print(i)
+        labels = t[0].unsqueeze(0)
+        _data = t[1:]
+        _data = replace_nans(downsample(_data))
+        tensor[i] = torch.cat((labels, _data))
+    return tensor
+
+
+def split_tensor(tensor, sampling_rate=100, selection_length=3):
+    """
+    Splits every input tensor into multiple usable sections.
+    Outputs a composite tensor that contains trainable samples.
+    Gets faster as dsf increases in downsample_task_tensor().
+    """
+    selection_samples = int(sampling_rate * selection_length)
+    labels = [data[0] for data in tensor]
+    selections = [data[1:, :] for data in tensor]
+    data_tensors = []
+    for j, selection in enumerate(selections):
+        for i in range(0, len(selection) - selection_samples, selection_samples):
+            end = i + selection_samples
+            _slice = selection[i:end]
+            if _slice is not False:
+                _labels = labels[j].unsqueeze(0)
+                # _slice = downsample(_slice)
+                # _slice = replace_nans(_slice)
+                data_tensors.append(torch.cat((_labels, _slice)).unsqueeze(0))
+            else:
+                print("Selection {} rejected".format(j))
+    return torch.cat(data_tensors)
+
+
+@alert_deprication
+def split_ds_tensor(tensor, sampling_rate=100, selection_length=3):
+    selection_samples = int(sampling_rate * selection_length)
+    label = tensor[0].unsqueeze(0)
+    data = tensor[1:]
+    print(label.shape, data.shape)
+    data_t = []
+    for i in range(0, len(data) - selection_samples, selection_samples):
+        end = i + selection_samples
+        _slice = data[i:end]
+        if _slice is not False:
+            data_t.append(torch.cat((label, _slice)).unsqueeze(0))
+        else:
+            print("Selection rejected")
+    return data_t
+
+
+@time_it
+def return_data_loaders(files, train_idxs, test_idxs, batch_size=64):
+    train = files[:-train_idxs]
+    test = files[-test_idxs:]
+    train_data_tensors = []
+    test_data_tensors = []
+    print("Loading training data")
+    for file in train:
+        print(file[-15:-10])
+        split_tensors = split_tensor(torch.load(file))
+        train_data_tensors.append(split_tensors)
+        # name_string = "DS10_{}_tensor.pt".format(file[-15:-10])
+        # torch.save(torch.cat(split_tensors), name_string)
+    print("Loading testing data")
+    for file in test:
+        print(file[-15:-10])
+        split_tensors = split_tensor(torch.load(file))
+        test_data_tensors.append(split_tensors)
+        # name_string = "DS10_{}_tensor.pt".format(file[-15:-10])
+        # torch.save(torch.cat(split_tensors), name_string)
+        # print(len(test_data_tensors[:, 0]))
+    # for file in train:
+    #     if train_data_tensors is None:b
+    #         train_data_tensors = split_tensor(torch.load(file))
+    #     else:
+    #         train_data_tensors = torch.cat((train_data_tensors, split_tensor(torch.load(file))), dim=0)
+    #     print(np.shape(train_data_tensors), file[-15:-10])
+    # for file in test:
+    #     if test_data_tensors is None:
+    #         test_data_tensors = split_tensor(torch.load(file))
+    #     else:
+    #         test_data_tensors = torch.cat((test_data_tensors, split_tensor(torch.load(file))), dim=0)
+    #     print(np.shape(test_data_tensors), file[-15:-10])
+    train_dataloader = DataLoader(torch.cat(train_data_tensors), batch_size=batch_size, shuffle=True)
+    test_dataloader = DataLoader(torch.cat(test_data_tensors), batch_size=batch_size, shuffle=True)
+    return train_dataloader, test_dataloader
+
+
+if __name__ == "__main__":
+    """@Ippa"""
+    # NOTE: Please teach me how to do this elegantly:
+    folder = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'Pipeline', 'tensors'))
+    files = list_files(folder)
+    # Thank you.
+
+    for file in files:
+        tensor = downsample_task_tensor(file)
+        name_string = "DS10_{}_tensor.pt".format(file[-15:-10])
+        torch.save(tensor, name_string)
+
+
+    # train_dataloader, test_dataloader = return_data_loaders(files, 125, 2, 64)
+    # # Train test split based on task. Eval acc per task to select interests.
+    # train_sample = next(iter(train_dataloader))
+    # test_sample = next(iter(test_dataloader))
+    # print(train_sample.shape, test_sample.shape)
+    # for sample in train_sample:
+    #     print(sample)
