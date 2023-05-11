e1cd057a4165a564175aca69d402c2752b0c7482
Tenzing Dolmans <t.c.dolmans@gmail.com>
2023-05-11 12:32:42 +0300

    new function to load img and sem data

--- pipeline/utils.py
+++ pipeline/utils.py
@@ -6,8 +6,12 @@
  * @desc [description]
 """
 import os
-import scipy.io as sio
+import glob
+import torch
+import scipy
 import numpy as np
+from PIL import Image
+import scipy.io as sio
 
 
 def list_files(directory):
@@ -33,6 +37,43 @@ def retrieve_semantic_label(file):
     mat = sio.loadmat(file)
     mat = np.array(mat['data'])
     return mat
+
+
+def load_img_sem_data(img_folder='OSIE_imgs',
+                      sem_folder='OSIE_tags',
+                      img_extension='*.jpg',
+                      sem_extension='*.mat'):
+    """
+    Load images and semantic labels from the OSIE dataset.
+    Inputs:
+    - img_folder: folder containing the images
+    - sem_folder: folder containing the semantic labels
+    Outputs:
+    - img_tensor_all: tensor containing all images
+    - sem_tensor_all: tensor containing all semantic labels
+    """
+    img_paths = sorted(glob.glob(os.path.join(img_folder, img_extension)))
+    sem_paths = sorted(glob.glob(os.path.join(sem_folder, sem_extension)))
+    img_tensors = []
+    sem_tensors = []
+
+    for img_path, sem_path in zip(img_paths, sem_paths):
+        # Load image and convert to tensor
+        img = Image.open(img_path).convert('RGB')
+        img_tensor = torch.from_numpy(np.array(img)).permute(2, 0, 1).float()
+
+        # Load semantic labels and convert to tensor
+        sem_mat = scipy.io.loadmat(sem_path)
+        sem_tensor = torch.tensor(sem_mat['data'], dtype=torch.float32)
+
+        img_tensors.append(img_tensor)
+        sem_tensors.append(sem_tensor)
+
+    # Concatenate tensors along the first dimension
+    img_tensor_all = torch.stack(img_tensors, dim=0)
+    sem_tensor_all = torch.stack(sem_tensors, dim=0)
+
+    return img_tensor_all, sem_tensor_all
 
 
 if __name__ == "__main__":
