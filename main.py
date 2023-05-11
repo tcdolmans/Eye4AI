1c4ca18c620a634bbdd9e332736329c2e4eb564c
Tenzing <63001022+tcdolmans@users.noreply.github.com>
2023-05-11 12:38:38 +0300

    Delete main.py

--- main.py
+++
@@ -1,5 +0,0 @@
-"""
-Contains the main loop for the entire project. Different parts can be called
-to carry out various parts of the pipeline.For exmaple, data curation does
-not need to happen everytime and should only be run once.
-"""
